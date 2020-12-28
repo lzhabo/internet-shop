1209522a9671f82fbe1f4ab8c487f9cac4f0a403
Лидия <lidia@MacBook-Pro-Lidia.local>
2020-12-28 02:06:24 +0200

    added product page form

--- frontend/src/components/ProductPage.tsx
+++ frontend/src/components/ProductPage.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { useObserver } from "mobx-react-lite";
 import { useParams } from "react-router-dom";
 import { useStores } from "@stores";
-import ProductCard from "@components/ProductCard";
+import { Column } from "./flex";
+import Title from "@components/Title";
+import Loading from "@components/Loading";
+import Page404 from "@components/Page404";
+import { useForm } from "react-hook-form";
 
 interface IProps {}
 
@@ -16,16 +20,36 @@ interface ParamIds {
   id: string;
 }
 
-const Img = styled.img``;
+interface BasketValues {
+  id: string;
+  amount: number;
+  material: string;
+}
+
+const Img = styled.img`
+  width: 300px;
+  height: 300px;
+`;
 const ProductPage: React.FC<IProps> = () => {
   const { id } = useParams<ParamIds>();
   const { productStore } = useStores();
 
+  const initialized = useObserver(() => productStore.initialized);
   const products = useObserver(function () {
     return productStore.products;
   });
   const product = products.find((p) => p._id === id);
-  console.log("product page");
+
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (v: BasketValues) => {
+    console.log(v);
+  };
+  if (!initialized)
+    return (
+      <Root>
+        <Loading />
+      </Root>
+    );
   if (product !== undefined)
     return (
       <Root>
@@ -34,8 +58,40 @@ const ProductPage: React.FC<IProps> = () => {
         ) : (
           <div></div>
         )}
+        <Column>
+          <Title>
+            "{product.name}" {product.type}
+          </Title>
+          <Title>$ {product.price}</Title>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <select name="material" ref={register}>
+              <option value="">Choose...</option>
+              {product.material.map((m, index) => (
+                <option value={m}>{m.toUpperCase()}</option>
+              ))}
+            </select>
+            <input
+              name="amount"
+              type="number"
+              ref={register({ pattern: /\d+/ })}
+            />
+            <input
+              type="hidden"
+              id="productId"
+              name="id"
+              value={product._id}
+              ref={register}
+            />
+            <input type="submit" />
+          </form>
+        </Column>
       </Root>
     );
-  else return <Root></Root>;
+  else
+    return (
+      <Root>
+        <Page404 />
+      </Root>
+    );
 };
 export default ProductPage;
